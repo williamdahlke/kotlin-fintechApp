8df08e6b6ccb45876be72c148dd170e9ad9f87bb
William Guilherme Dahlke <williamgdahlke@gmail.com>
2024-12-19 22:20:22 -0300

    feat: add CadastrarActivity and ExtratoActivity. Upload fintech image

---
+++ app/src/main/java/dahlke/br/fintechapp/ExtratoActivity.kt
@@ -0,0 +1,20 @@
+package dahlke.br.fintechapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ExtratoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_extrato)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
